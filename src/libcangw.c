d4ee92279c08d94ddf2e50a9b71cf9fb8a8765c2
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-03-29 21:29:10 +0900

    Add cangw_clean_rule function

--- src/libcangw.c
+++ src/libcangw.c
@@ -199,3 +199,45 @@ int cangw_delete_rule(socketcan_gw_rule_t *rule)
 do_return:
 	return result;
 }
+
+/**
+ * @ingroup extern
+ * cangw_clean_rule - delete routing rule to can gateway
+ * @param src_ifindex structure of the can gateway.
+ * @param dst_ifindex structure of the can gateway.
+ *
+ * @return 0 if success
+ * @return -1 if operation is failed
+ * @return -2 if linux does not support can gateway
+ * @return -3 if argument is invalid
+ */
+int cangw_clean_rule(unsigned int src_ifindex, unsigned int dst_ifindex)
+{
+	int result = 0;
+	int ret = -1;
+	struct s_request_data req;
+
+	// Setup common message
+	memset(&req, 0, sizeof(req));
+
+	req.nh.nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK;
+	req.nh.nlmsg_type  = RTM_DELROUTE;
+	req.nh.nlmsg_len   = NLMSG_LENGTH(sizeof(struct rtcanmsg));
+	req.nh.nlmsg_seq   = 0;
+
+	req.rtcan.can_family  = AF_CAN;
+	req.rtcan.gwtype = CGW_TYPE_CAN_CAN;
+	req.rtcan.flags = 0;
+
+	addattr_l(&req.nh, sizeof(req), CGW_SRC_IF, &src_ifindex, sizeof(src_ifindex));
+	addattr_l(&req.nh, sizeof(req), CGW_DST_IF, &dst_ifindex, sizeof(dst_ifindex));
+
+	ret = send_cangw_request(&req);
+	if (ret < 0) {
+		result = -1;
+		goto do_return;
+	}
+
+do_return:
+	return result;
+}
