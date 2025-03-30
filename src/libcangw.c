f947146799309bfd55808dc453137245ced2c2b9
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-03-31 01:13:34 +0900

    WIP: cangw_get_rules

--- src/libcangw.c
+++ src/libcangw.c
@@ -80,88 +80,155 @@ do_return:
 	return result;
 }
 
-static int parse_rtlist(unsigned char *rxbuf, int len)
+static int push_gw_rule(socketcan_gw_rules_t *gw_rules, socketcan_gw_rule_t *rule)
+{
+	int result = 0;
+
+	if (gw_rules->rules == NULL) {
+		// Create rules array
+		gw_rules->rule_num = 0;	// Initial size
+		gw_rules->array_num = 2;	// Initial size
+		gw_rules->rules = (socketcan_gw_rule_t**)malloc(sizeof(socketcan_gw_rule_t*) * gw_rules->array_num);
+		if (gw_rules->rules == NULL) {
+			result = -1;
+			goto do_return;
+		}
+	}
+
+	if (!(gw_rules->rule_num < gw_rules->array_num)) {
+		// Extend array
+		socketcan_gw_rule_t **pnew_rules = NULL;
+		gw_rules->array_num = gw_rules->array_num * 2;
+		pnew_rules = (socketcan_gw_rule_t**)realloc(gw_rules->rules, (sizeof(socketcan_gw_rule_t*) * gw_rules->array_num));
+		if (pnew_rules != NULL) {
+			gw_rules->rules = pnew_rules;
+		} else {
+			result = -1;
+			goto do_return;
+		}
+	}
+
+	gw_rules->rules[gw_rules->rule_num] = rule;
+	gw_rules->rule_num = gw_rules->rule_num + 1;
+
+do_return:
+	return result;
+}
+
+static int free_gw_rules(socketcan_gw_rules_t *gw_rules)
+{
+	for(size_t i=0; i < gw_rules->rule_num; i++) {
+		free(gw_rules->rules[i]);
+		gw_rules->rules[i] = NULL;
+	}
+
+	free(gw_rules->rules);
+	gw_rules->rules = NULL;
+	gw_rules->rule_num = 0;
+	gw_rules->array_num = 0;
+
+	return 0;
+}
+
+static int print_gw_rules(socketcan_gw_rules_t *gw_rules)
 {
 	char src_ifname[IF_NAMESIZE]; /* interface name for if_indextoname() */
 	char dst_ifname[IF_NAMESIZE]; /* interface name for if_indextoname() */
+
+	for(size_t i=0; i < gw_rules->rule_num; i++) {
+		socketcan_gw_rule_t *rule = gw_rules->rules[i];
+
+		fprintf(stdout, "cangw: -s %s -d %s -f %03X:%X\n", 
+			if_indextoname(rule->src_ifindex, src_ifname), 
+			if_indextoname(rule->dst_ifindex, dst_ifname),
+			rule->filter.can_id,
+			rule->filter.can_mask);
+	}
+
+	return 0;
+}
+
+static int parse_listing_data(socketcan_gw_rules_t *gw_rules, unsigned char *rxbuf, int len)
+{
 	struct rtcanmsg *rtc;
 	struct rtattr *rta;
 	struct nlmsghdr *nlh;
 	int rtlen;
-	socketcan_gw_rule_t rule;
+	socketcan_gw_rule_t *rule = NULL;
+	int result = 0;
 
 	nlh = (struct nlmsghdr *)rxbuf;
 
 	while (1) {
-		printf("HOGE:");
 		if (!NLMSG_OK(nlh, len)){
-			printf("!NLMSG_OK\n");
-			return 0;
+			result = 0;
+			break;
 		}
 
 		if (nlh->nlmsg_type == NLMSG_ERROR) {
-			printf("NLMSG_ERROR\n");
-			return 1;
+			result = -1;
+			break;
 		}
 
 		if (nlh->nlmsg_type == NLMSG_DONE) {
-			printf("NLMSG_DONE\n");
-			return 1;
+			result = 1;
+			break;
 		}
 
-		memset(&rule, 0 ,sizeof(rule));
 		rtc = (struct rtcanmsg *)NLMSG_DATA(nlh);
 		if (rtc->can_family != AF_CAN) {
-			printf("received msg from unknown family %d\n", rtc->can_family);
-			return -EINVAL;
+			result = -1;
+			break;
 		}
 
 		if (rtc->gwtype != CGW_TYPE_CAN_CAN) {
-			printf("received msg with unknown gwtype %d\n", rtc->gwtype);
-			return -EINVAL;
+			result = -1;
+			break;
 		}
 
-		/* first parse for mandatory options */
+		rule = (socketcan_gw_rule_t*)malloc(sizeof(socketcan_gw_rule_t));
+		if (rule == NULL) {
+			result = -2;
+			goto error_return;
+		}
+		memset(rule, 0 ,sizeof(socketcan_gw_rule_t));
+
 		rta = (struct rtattr *) RTCAN_RTA(rtc);
 		rtlen = RTCAN_PAYLOAD(nlh);
-		for(;RTA_OK(rta, rtlen);rta=RTA_NEXT(rta,rtlen))
-		{
+		for(; RTA_OK(rta, rtlen); rta=RTA_NEXT(rta,rtlen)) {
 			switch(rta->rta_type) {
 			case CGW_SRC_IF:
-				rule.src_ifindex = (*(unsigned int*)RTA_DATA(rta));
+				rule->src_ifindex = (*(unsigned int*)RTA_DATA(rta));
 				break;
-
 			case CGW_DST_IF:
-				rule.dst_ifindex = (*(unsigned int*)RTA_DATA(rta));
+				rule->dst_ifindex = (*(unsigned int*)RTA_DATA(rta));
 				break;
 			default:
 				break;
 			}
 		}
 
-		rule.options = (SOCKETCAN_GW_RULE_ECHO | SOCKETCAN_GW_RULE_FILTER);
+		rule->options = (SOCKETCAN_GW_RULE_ECHO | SOCKETCAN_GW_RULE_FILTER);
 
 		if ((rtc->flags & CGW_FLAGS_CAN_ECHO) == CGW_FLAGS_CAN_ECHO) {
-			rule.options = 1;
+			rule->options = 1;
 		} else {
-			rule.options = 0;
+			rule->options = 0;
 		}
 
-		/* second parse for mod attributes */
 		rta = (struct rtattr *) RTCAN_RTA(rtc);
 		rtlen = RTCAN_PAYLOAD(nlh);
-		for(;RTA_OK(rta, rtlen);rta=RTA_NEXT(rta,rtlen))
-		{
+		for(; RTA_OK(rta, rtlen); rta=RTA_NEXT(rta,rtlen)) {
 			switch(rta->rta_type) {
 			case CGW_FILTER:
 			{
 				struct can_filter *filter = (struct can_filter *)RTA_DATA(rta);
 				if (filter->can_id & CAN_INV_FILTER) {
-					rule.filter.can_id = (filter->can_id & ~CAN_INV_FILTER);
+					rule->filter.can_id = (filter->can_id & ~CAN_INV_FILTER);
 				} else {
-					rule.filter.can_id = filter->can_id;
+					rule->filter.can_id = filter->can_id;
 				}
-				rule.filter.can_mask = filter->can_mask;
+				rule->filter.can_mask = filter->can_mask;
 				break;
 			}
 
@@ -171,63 +238,19 @@ static int parse_rtlist(unsigned char *rxbuf, int len)
 		}
 		/* end of entry */
 
-		printf("cangw: -s %s -d %s -f %03X:%X\n", 
-			if_indextoname(rule.src_ifindex, src_ifname), 
-			if_indextoname(rule.dst_ifindex, dst_ifname),
-			rule.filter.can_id,
-			rule.filter.can_mask);
+		push_gw_rule(gw_rules, rule);
 
 		/* jump to next NLMSG in the given buffer */
 		nlh = NLMSG_NEXT(nlh, len);
 	}
-}
-
-static int send_cangw_get_request(struct s_request_data *req)
-{
-	int result = 0;
-	int sock_fd = -1;
-	ssize_t ret = -1;
-	struct sockaddr_nl nladdr;
-	unsigned char rxbuf[8192];
-
-	// Open netlink socket interface
-	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-	if (sock_fd < 0) {
-		result = -1;
-		goto do_return;
-	}
-
-	memset(&nladdr, 0, sizeof(nladdr));
-	nladdr.nl_family = AF_NETLINK;
-	nladdr.nl_pid    = 0;
-	nladdr.nl_groups = 0;
-
-	ret = sendto(sock_fd, req, req->nh.nlmsg_len, 0, (struct sockaddr*)&nladdr, sizeof(nladdr));
-	if (ret < 0) {
-		result = -1;
-		goto do_return;
-	}
-
-	memset(rxbuf, 0, sizeof(rxbuf));
-	while (1) {
-		ret = recv(sock_fd, &rxbuf, sizeof(rxbuf), 0);
-		if (ret < 0) {
-			result = -1;
-			goto do_return;
-		}
-
-		/* leave on errors or NLMSG_DONE */
-		if (parse_rtlist(rxbuf, ret))
-			break;
-	}
-
-do_return:
-	if (sock_fd >= 0) {
-		close(sock_fd);
-	}
 
 	return result;
+
+error_return:
+	free(rule);
+	return result;
 }
+
 /**
  * @ingroup extern
  * cangw_add_rule - add routing rule to can gateway
@@ -396,25 +419,27 @@ do_return:
 /**
  * @ingroup extern
  * cangw_delete_rule - delete routing rule to can gateway
- * @param rule rule structure of the can gateway.
+ * @param gw_rules rule structure of the can gateway.
  *
  * @return 0 if success
  * @return -1 if operation is failed
  * @return -2 if linux does not support can gateway
  * @return -3 if argument is invalid
  */
-int cangw_get_rules(socketcan_gw_rule_t **rules, size_t *rule_num)
+int cangw_get_rules(socketcan_gw_rules_t **gw_rules)
 {
 	int result = 0;
-	int ret = -1;
+	int sock_fd = -1;
+	ssize_t ret = -1;
 	struct s_request_data req;
+	struct sockaddr_nl nladdr;
+	socketcan_gw_rules_t *pgw_rules = NULL;
 
-	if ((rules == NULL) || (rule_num == NULL)) {
+	if (gw_rules == NULL) {
 		result = -3;
 		goto do_return;
 	}
 
-	// Setup common message
 	memset(&req, 0, sizeof(req));
 
 	req.nh.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
@@ -426,14 +451,79 @@ int cangw_get_rules(socketcan_gw_rule_t **rules, size_t *rule_num)
 	req.rtcan.gwtype = CGW_TYPE_CAN_CAN;
 	req.rtcan.flags = 0;
 
-	ret = send_cangw_get_request(&req);
+	// Open netlink socket interface
+	sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (sock_fd < 0) {
+		result = -2;
+		goto do_return;
+	}
+
+	memset(&nladdr, 0, sizeof(nladdr));
+	nladdr.nl_family = AF_NETLINK;
+	nladdr.nl_pid    = 0;
+	nladdr.nl_groups = 0;
+
+	ret = sendto(sock_fd, &req, req.nh.nlmsg_len, 0, (struct sockaddr*)&nladdr, sizeof(nladdr));
 	if (ret < 0) {
 		result = -1;
 		goto do_return;
 	}
 
+	pgw_rules = malloc(sizeof(socketcan_gw_rules_t));
+	if (pgw_rules == NULL) {
+		result = -1;
+		goto do_return;
+	}
 
+	pgw_rules->rule_num = 0;
+	pgw_rules->array_num = 0;
+	pgw_rules->rules = NULL;
 
+	while (1) {
+		unsigned char rxbuf[8192];
+		memset(rxbuf, 0, sizeof(rxbuf));
+
+		ret = recv(sock_fd, &rxbuf, sizeof(rxbuf), 0);
+		if (ret < 0) {
+			result = -1;
+			goto do_return;
+		}
+
+		/* leave on errors or NLMSG_DONE */
+		if (parse_listing_data(pgw_rules, rxbuf, ret))
+			break;
+	}
+	print_gw_rules(pgw_rules);
+	(*gw_rules) = pgw_rules;
+
+do_return:
+	if (sock_fd >= 0) {
+		close(sock_fd);
+	}
+
+	return result;
+}
+/**
+ * @ingroup extern
+ * cangw_delete_rule - delete routing rule to can gateway
+ * @param gw_rules rule structure of the can gateway.
+ *
+ * @return 0 if success
+ * @return -1 if operation is failed
+ * @return -2 if linux does not support can gateway
+ * @return -3 if argument is invalid
+ */
+int cangw_release_rules(socketcan_gw_rules_t *gw_rules)
+{
+	int result = 0;
+
+	if (gw_rules == NULL) {
+		result = -3;
+		goto do_return;
+	}
+
+	(void) free_gw_rules(gw_rules);
+	(void) free(gw_rules);
 
 do_return:
 	return result;
