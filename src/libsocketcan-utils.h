8e7b72a06c0eb9298e081ad4fd514811bc37b265
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-03-31 23:54:20 +0900

    Remove auther from libsocketcan-utils.x

--- src/libsocketcan-utils.h
+++ src/libsocketcan-utils.h
@@ -1,7 +1,6 @@
 /* libsocketcan-utils.h
  *
  * (C) 2009 Luotao Fu <l.fu@pengutronix.de>
- * (C) 2025 Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
