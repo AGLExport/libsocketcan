9b6623dd24dfc2060c8cff80b7c26235827c7038
Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
2025-04-02 02:37:32 +0900

    Fix license

--- src/libcangw.c
+++ src/libcangw.c
@@ -1,21 +1,33 @@
+/* libcangw.c
+ *
+ * (C) 2025 Naoto Yamaguchi <naoto.yamaguchi@aisin.co.jp>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/**
+ * @file
+ * @brief Socket CAN gateway library
+ */
+
 #include <linux/can/gw.h>
 #include <sys/socket.h>
 
 #include "libsocketcan-utils.h"
 
 #include <libsocketcangw.h>
-
-struct modattr {
-	struct can_frame cf;
-	__u8 modtype;
-	__u8 instruction;
-} __attribute__((packed));
-
-struct fdmodattr {
-	struct canfd_frame cf;
-	__u8 modtype;
-	__u8 instruction;
-} __attribute__((packed));
 
 struct s_request_data {
 	struct nlmsghdr nh;
